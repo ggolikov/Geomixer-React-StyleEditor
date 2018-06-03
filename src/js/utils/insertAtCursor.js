c6fcde257f259f634ff15f3af8981421635d0034
ggolikov <gr.golikov@gmail.com>
2018-06-03 21:48:31 +0300

    Add space by incering

--- src/js/utils/insertAtCursor.js
+++ src/js/utils/insertAtCursor.js
@@ -12,20 +12,23 @@ const insertAtCursor = (e, attrsValueWrapper, textArea, type) => {
             value = `"${value}"`;
         }
     }
-       if (document.selection) {
-           textArea.focus();
-           let sel = document.selection.createRange();
-           sel.text = value;
-       } else if (textArea.selectionStart || textArea.selectionStart == '0') {
-           let startPos = textArea.selectionStart,
-               endPos = textArea.selectionEnd;
 
-            textArea.value = textArea.value.substring(0, startPos) + value + textArea.value.substring(endPos, textArea.value.length);
-       } else {
-           textArea.value += myValue;
-       }
+    value = value + ' ';
 
-       reactTriggerChange(textArea);
+    if (document.selection) {
+        textArea.focus();
+        let sel = document.selection.createRange();
+        sel.text = value;
+    } else if (textArea.selectionStart || textArea.selectionStart == '0') {
+        let startPos = textArea.selectionStart,
+            endPos = textArea.selectionEnd;
+
+        textArea.value = textArea.value.substring(0, startPos) + value + textArea.value.substring(endPos, textArea.value.length);
+    } else {
+        textArea.value += myValue;
+    }
+
+    reactTriggerChange(textArea);
 };
 
 export default insertAtCursor;
